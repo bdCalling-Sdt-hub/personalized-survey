3804d97a6605a24ceba0f5d49777ce46a5b834ac
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-01-30 19:50:40 +0600

    Update vite.config.js
    url and port updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '46.202.89.63', // Exposes to local network (optional)
+    port: 3000, // Change this to your desired port
+  },
 })
